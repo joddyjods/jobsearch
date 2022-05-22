8d03e666210de741ae5781e335a15d0959a4cf9e
Andrew Wagner <andrew.wagner.pdx@gmail.com>
2022-05-22 16:35:27 -0700

    Created a conversation view

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+class Conversation extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      /*history: [
+        {
+          squares: Array(9).fill(null)
+        }
+      ],*/
+      interactions: [
+        {from: 'you', date: 'May 4, 2022', msg: 'Applied at the website', key:'abc'},
+        {from: 'Charles English', date: 'May 5, 2022', msg: 'Responded with an email', key:'123'},
+        {from: 'you', date: 'May 6, 2022', msg: 'Provided times for an interview', key:'asdf'}
+      ]
+    };
+  }
+
+  dataEnterNewMessage() {
+    this.addMessage( 'you', 'May 9, 2022', 'Did some other things','92fjdi' );
+  }
+
+  addMessage( who, when, msg, key ) {
+    let newstate = this.state;
+    newstate.interactions.push( {from: who, date: when, msg: msg, key: key } );
+    this.setState( newstate );
+  }
+
+  render() {
+    const interactions = this.state.interactions;
+    let i = 0;
+
+    let output = interactions.map(interaction => {
+      let theclass = 'ConversationLineRight';
+      if ( interaction.from == 'you' ) {
+        theclass = 'ConversationLineLeft';
+      }
+      let key = 'ConversationLine' + i++;
+      return ( 
+        <div className={theclass} key={key}>
+          <div className='Message' key={interaction.key}>
+            {interaction.date} <br />
+            {interaction.from} 
+            <p>
+              {interaction.msg}
+            </p>
+          </div>
+        </div> );
+    });
+
+    return ( 
+      <div className="ConversationInner"> {output} 
+        <div className='AddOne' onClick={() =>this.dataEnterNewMessage()}><p>+ Add a thing</p></div>
+      </div>
+      );
+  }
+
+  
+}
+
+class Interaction extends React.Component {
+  render() {
+    return "hi";
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Conversation">
+      <Conversation />
     </div>
   );
 }
